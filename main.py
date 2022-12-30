ba9d94b844857547f6568cf1234e554b74984671
nouranessam <juviaessam@gmail.com>
2022-12-30 20:06:48 +0200

    knn done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ The main module. Execution starts here
 """
 import json
 
-from test import logistic_regression, ada_boost
+from test import logistic_regression, ada_boost, knn
 
 if __name__ == '__main__':
     results = {
@@ -16,6 +16,11 @@ if __name__ == '__main__':
             '1-customer-churn.csv': ada_boost.test_1_ada_boost(),
             '2-hr-data.csv': ada_boost.test_2_ada_boost(),
             '3-telco-customer-churn.csv': ada_boost.test_3_ada_boost()
+        }, 
+         'KNeighborsClassifier': {
+            '1-customer-churn.csv': knn.test_1_knn(),
+            '2-hr-data.csv': knn.test_2_knn(),
+            '3-telco-customer-churn.csv': knn.test_3_knn()
         }
     }
     with open('out\\test-results\\all-results.json', 'w', encoding='utf-8') as f:
