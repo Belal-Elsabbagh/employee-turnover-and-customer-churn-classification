48e87eaeaeb482948110f540ff9f79b71d73ee5d
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-30 20:14:59 +0200

    formats

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ The main module. Execution starts here
 """
 import json
 
-
-
-
 from test import logistic_regression, ada_boost, decision_tree, random_forest, knn
-
 
 if __name__ == '__main__':
     results = {
@@ -20,13 +16,11 @@ if __name__ == '__main__':
             '1-customer-churn.csv': ada_boost.test_1_ada_boost(),
             '2-hr-data.csv': ada_boost.test_2_ada_boost(),
             '3-telco-customer-churn.csv': ada_boost.test_3_ada_boost()
-
-        }, 
-         'KNeighborsClassifier': {
+        },
+        'KNeighborsClassifier': {
             '1-customer-churn.csv': knn.test_1_knn(),
             '2-hr-data.csv': knn.test_2_knn(),
             '3-telco-customer-churn.csv': knn.test_3_knn()
-
         },
         'DecisionTreeClassifier': {
             '1-customer-churn.csv': decision_tree.test_1_decision_tree(),
@@ -37,7 +31,6 @@ if __name__ == '__main__':
             '1-customer-churn.csv': random_forest.test_1_random_forest(),
             '2-hr-data.csv': random_forest.test_2_random_forest(),
             '3-telco-customer-churn.csv': random_forest.test_3_random_forest()
-
         }
     }
     with open('out\\test-results\\all-results.json', 'w', encoding='utf-8') as f:
